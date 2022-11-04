33ef292baee53959ebd1fd4a20894e50e97400b0
Giacomo <50072283+giacomodeconti@users.noreply.github.com>
2022-11-04 18:07:30 +0100

    Update game2.0.c

--- Week2/Quiz game C/game2.0.c
+++ Week2/Quiz game C/game2.0.c
@@ -2,10 +2,11 @@
 #include<stdlib.h>
 int main (){
 	while(1==1){
+		int contr;
 		char name[20];// name lenght check max 20
 		int point=0; // point
 		int val; // input value
-		printf ("Quiz game\nFor start press 1  otherwise  digit any key\n"); // game start or shut down
+		printf ("Quiz game\nFor start press 1  otherwise  digit any key to exit\n"); // game start or shut down
 		scanf ("%d",&val);
 		if (val==1){
 			printf("Type your name ");
@@ -14,24 +15,32 @@ int main (){
   			printf("\nYour name is : %s\n\nPRESS ENTER to start", name); // name print
   			getch(); // starts game
   			system("cls"); //clear command line
+  			do{ // initial loop fro answer check
 			printf ("Which is the first letter in afabet?\n\nChoose:\n\n1) b\n2) c\n3) a\n"); // first question
 			rewind(stdin); // clear buffer
-			scanf ("%d",&val); // input
+			contr=scanf ("%d",&val); // input
 			if (val==3){
 				point++;} // point +1
 			else {}
+			}while(val<1||val>3||contr==0); // rule for do and answre check			
 			system("cls");
+			do{ // initial loop fro answer check
+			rewind(stdin); // clear buffer
 			printf ("Which is the second letter in afabet?\n\nChoose:\n\n1) b\n2) c\n3) a\n"); // second question
-			scanf  ("%d",&val); // input
+			contr=scanf  ("%d",&val); // input
 			if (val==1){
 				point++;} // point +1
 			else {}
+			}while(val<1||val>3||contr==0); // rule for do and answre check
 			system("cls");
+			do{ // initial loop fro answer check
+			rewind(stdin); // clear buffer
 			printf ("1+2=?\n\nChoose:\n\n1) 1\n2) 2\n3) 3\n"); // third question
-			scanf  ("%d",&val); // input
+			contr=scanf  ("%d",&val); // input
 			if (val==3){
 				point++;} // point +1
 			else{}
+			}while(val<1||val>3||contr==0); // rule for do and answre check
 			printf("\n\nHI! %s", name); // print name
 			printf ("\nYOU GOT %d POINTS\n\n",point); // print results
 			printf ("\nPRESS ANY KEY TO CONTINUE\n\n");
@@ -42,7 +51,3 @@ int main (){
 			printf ("installing WannaCry...\n\nSend me some bitcoin");
 			break;}} // stop program
 return 0;}
-		
-
-
-	
